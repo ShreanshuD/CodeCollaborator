50a6d8ba93a365b123a78658655e9a56d1c200be
asmita18ashii <asmita18ashii@gmail.com>
2024-01-03 01:19:11 +0530

    chatBox

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import AuthenticationPage from './components/Login/Page'
 import Form from './components/Create/Form'
+import {ChatComponent} from './components/Socket/chatbox'
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<AuthenticationPage />} />
 			<Route path="/create" element={<Form />} />
+			<Route path='/chatbox' element={<ChatComponent/>}/>
 		</Routes>
 	)
 }
--- Frontend/src/components/Socket/chatbox.tsx
+++ Frontend/src/components/Socket/chatbox.tsx
@@ -1,46 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import socket from '../../assets/js/Socket';
 // Adjust the path based on your project structure
+import { io } from 'socket.io-client';
 
-const ChatComponent = () => {
-    const [messages, setMessages] = useState<string[]>([]);
+const socket = io('http://localhost:3001');
 
+export function ChatComponent() {
+    const [message,setMessage]=useState("")
+    const [receivedMessage,setReceivedMessage]=useState("")
+    const [room,setRoom]=useState('')
 
-
-  useEffect(() => {
-    // Listen for 'chat message' events
-    socket.on('chat message', (message: string) => {
-      // Use the correct type for the state update
-      setMessages((prevMessages: string[]) => [...prevMessages, message]);
-    });
-  
-    // Clean up the event listener on component unmount
-    return () => {
-      socket.off('chat message');
+    const joinRoom=()=>{
+        
+        if(room!=''){
+            socket.emit("join_room",room)
+        }
+    }
+    const sendMessage = () => {
+        setRoom('99')
+        joinRoom()
+        // Assuming messages is an array of strings
+        socket.emit('send_messages',{message,room})
     };
-  }, []);
 
-  const sendMessage = () => {
-    // Emit 'chat message' event
-    socket.emit('chat message', messages);
-    setMessages([]);
-  };
+    useEffect(()=>{
+        socket.on("receive_message",(data:any)=>{
+            setReceivedMessage(data.message);
+        })
+    },[])
 
   return (
-    <div>
-      <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        value={messages}
-        onChange={(e) => setMessages([e.target.value])}
-      />
-      <button onClick={sendMessage}>Send</button>
-    </div>
+   <div className='App'>
+    <input placeholder='message,,' onChange={(e)=>setMessage(e.target.value)}/>
+    <button onClick={sendMessage}>Send message</button>
+    <div>{receivedMessage}</div>
+   </div>
   );
-};
+}
 
-export default ChatComponent;
